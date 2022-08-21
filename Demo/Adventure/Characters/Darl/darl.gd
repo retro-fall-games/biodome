bebece5c20aa9c58a4311147cc257989bd5168a9
Andrew Bliss <andrewgbliss@gmail.com>
2022-08-21 15:11:29 -0600

    updates

--- Demo/Adventure/Characters/Darl/darl.gd
+++ Demo/Adventure/Characters/Darl/darl.gd
@@ -1,6 +1,12 @@
 extends CharacterBody2D
 
-@export var move_speed : Vector2 = Vector2(40, 40)
+@export var move_speed : Vector2 = Vector2(40, 20)
+@export var animation_idle_x : String = "IdleX"
+@export var animation_idle_up : String = "IdleUp"
+@export var animation_idle_down : String = "IdleDown"
+@export var animation_walk_x : String = "WalkX"
+@export var animation_walk_up : String = "WalkUp"
+@export var animation_walk_down : String = "WalkDown"
 
 @onready var animation_player = $AnimationPlayer
 
@@ -28,84 +34,80 @@ func _input(event):
 
 func _physics_process(delta):
 				
+	if Input.is_action_just_pressed("up"):
+		change_movement_state(MovementDirection.Up, 0, -1)
+
+	if Input.is_action_just_pressed("down"):
+		change_movement_state(MovementDirection.Down, 0, 1)
+
+	if Input.is_action_just_pressed("left"):
+		change_movement_state(MovementDirection.Left, -1, 0)
+
+	if Input.is_action_just_pressed("right"):
+		change_movement_state(MovementDirection.Right, 1, 0)
+
+	if Input.is_action_just_pressed("leftup"):
+		change_movement_state(MovementDirection.UpLeft, -1, -1)
+
+	if Input.is_action_just_pressed("rightup"):
+		change_movement_state(MovementDirection.UpRight, 1, -1)
+
+	if Input.is_action_just_pressed("leftdown"):
+		change_movement_state(MovementDirection.DownLeft, -1, 1)
+
+	if Input.is_action_just_pressed("rightdown"):
+		change_movement_state(MovementDirection.DownRight, 1, 1)
+					
 	move_and_slide()
+	
+	if get_slide_collision_count() > 0:
+		movement_state = MovementState.Idle
+		input.x = 0
+		input.y = 0
+		update_animations()
 	
 	if velocity.x > 0:
 		scale.x = scale.y * 1
 	elif velocity.x < 0:
 		scale.x = scale.y * -1
 		
-func detect_movement_state():
+func change_movement_state(move_direction : MovementDirection, x : float, y : float):
+	if (move_direction != movement_direction):
+		movement_state = MovementState.Walking
+		input.x = x
+		input.y = y
+		movement_direction = move_direction
+	elif (movement_state == MovementState.Walking):
+		movement_state = MovementState.Idle
+		input.x = 0
+		input.y = 0
+	else:
+		movement_state = MovementState.Walking
+		input.x = x
+		input.y = y
+		movement_direction = move_direction
+	velocity.x = input.x * move_speed.x
+	velocity.y = input.y * move_speed.y
+	update_animations()
+	
+func update_animations():
 	if (velocity.x > 0):
-		# _character.MovementState.ChangeState(typeof(WalkRightState));
-		print("right")
+		animation_player.play(animation_walk_x)
 	elif (velocity.x < 0):
-		# _character.MovementState.ChangeState(typeof(WalkLeftState));
-		print("left")
-	elif (velocity.y > 0):
-		# _character.MovementState.ChangeState(typeof(WalkUpState));
-		print("up")
+		animation_player.play(animation_walk_x)
 	elif (velocity.y < 0):
-		# _character.MovementState.ChangeState(typeof(WalkDownState));
-		print("down")
+		animation_player.play(animation_walk_up)
+	elif (velocity.y > 0):
+		animation_player.play(animation_walk_down)
 	else:
 		match movement_direction:
 			MovementDirection.Up:
-				# _character.MovementState.ChangeState(typeof(WalkUpIdleState));
-				print("up")
+				animation_player.play(animation_idle_up)
 			MovementDirection.Down:
-				#_character.MovementState.ChangeState(typeof(WalkDownIdleState));
-				print("down")
+				animation_player.play(animation_idle_down)
 			MovementDirection.Left:
-				#_character.MovementState.ChangeState(typeof(WalkLeftIdleState));
-				print("left")
+				animation_player.play(animation_idle_x)
 			MovementDirection.Right:
-				#_character.MovementState.ChangeState(typeof(WalkRightIdleState));
-				print("right")
+				animation_player.play(animation_idle_x)
 			_:
-				#_character.MovementState.ChangeState(typeof(IdleState));
-				print("idle")
-
-
-func change_movement_state(move_direction : MovementDirection, x : float, y : float):
-	if (move_direction != movement_direction):
-		movement_state = MovementState.Walking;
-		input.x = x;
-		input.y = y;
-		movement_direction = move_direction;
-	elif (movement_state == MovementState.Walking):
-		movement_state = MovementState.Idle;
-		input.x = 0;
-		input.y = 0;
-	else:
-		movement_state = MovementState.Walking;
-		input.x = x;
-		input.y = y;
-		movement_direction = move_direction;
-	velocity.x = input.x * move_speed.x
-	velocity.y = input.y * move_speed.y
-	detect_movement_state()
-	
-func OnWalkUp():
-	change_movement_state(MovementDirection.Up, 0, 1)
-
-func OnWalkDown():
-	change_movement_state(MovementDirection.Down, 0, -1)
-
-func OnWalkLeft():
-	change_movement_state(MovementDirection.Left, -1, 0)
-
-func OnWalkRight():
-	change_movement_state(MovementDirection.Right, 1, 0)
-
-func OnWalkUpLeft():
-	change_movement_state(MovementDirection.UpLeft, -1, 1)
-
-func OnWalkUpRight():
-	change_movement_state(MovementDirection.UpRight, 1, 1)
-
-func OnWalkDownLeft():
-	change_movement_state(MovementDirection.DownLeft, -1, -1)
-
-func OnWalkDownRight():
-	change_movement_state(MovementDirection.DownRight, 1, -1)
+				animation_player.play(animation_idle_x)
